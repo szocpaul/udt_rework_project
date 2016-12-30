72331d051a422d4d3bcd206245a0eac3b3daeb91
Pal Majoros <pal.majoros@hu.ibm.com>
2016-12-30 12:16:45 +0100

    Migrate Task and remove slug

--- udt/models.py
+++ udt/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-from udt.choices import REQUEST_TYPE_CHOICES, REQUEST_VIA_CHOICES, GEO_CHOICES, FILTER_BY_TOOL_CHOICES, SEVERITY_CHOICES
+from udt.choices import REQUEST_TYPE_CHOICES, REQUEST_VIA_CHOICES, GEO_CHOICES, FILTER_BY_TOOL_CHOICES, SEVERITY_CHOICES, TASKS_CHOICES
 
 
 class Dispatcher(models.Model):
@@ -9,9 +9,8 @@ class Dispatcher(models.Model):
     requested_via = models.CharField(max_length=20, choices=REQUEST_VIA_CHOICES)
     geo = models.CharField(max_length=15, choices=GEO_CHOICES)
     filter_by_tool = models.CharField(max_length=20, choices=FILTER_BY_TOOL_CHOICES)
-    task = models.CharField(max_length=250)
+    task = models.CharField(max_length=250, choices=TASKS_CHOICES)
     assign_to = models.ForeignKey(User, related_name='work_orders')
-    slug = models.SlugField(max_length=50, unique_for_date='task_received')
     task_received = models.DateTimeField(default=timezone.now)
     link = models.CharField(max_length=500)
     severity = models.CharField(max_length=5, choices=SEVERITY_CHOICES)
