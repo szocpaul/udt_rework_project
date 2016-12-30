c560aba21eda8e189a58beacf5293fa8e890e4f2
Pal Majoros <pal.majoros@hu.ibm.com>
2016-12-30 10:07:40 +0100

    First DB migration

---
+++ udt/choices.py
@@ -0,0 +1,36 @@
+REQUEST_TYPE_CHOICES = (
+    ('Change', 'Change'),
+    ('Incident', 'Incident'),
+    ('Problem', 'Problem'),
+    ('Other', 'Other'),
+)
+REQUEST_VIA_CHOICES = (
+    ('Change Support', 'Change Support'),
+    ('Incident Support', 'Incident Support'),
+    ('Problem Support', 'Problem Support'),
+    ('Notes', 'Notes'),
+    ('Sametime', 'Sametime'),
+    ('Phone', 'Phone'),
+    ('RTC', 'RTC'),
+    ('Other', 'Other'),
+)
+GEO_CHOICES = (
+    ('EMEA', 'EMEA'),
+    ('AG', 'AG'),
+)
+FILTER_BY_TOOL_CHOICES = (
+    ('URT', 'URT'),
+    ('ECM', 'ECM'),
+    ('ePolicy', 'ePolicy'),
+    ('CWP', 'CWP'),
+    ('SecInfo', 'SecInfo'),
+    ('Cirats DB2', 'Cirats DB2'),
+    ('Cirats Alps&Germany', 'Cirats Alps&Germany'),
+)
+SEVERITY_CHOICES = (
+    ('Sev 1', 'Sev 1'),
+    ('Sev 2', 'Sev 2'),
+    ('Sev 3', 'Sev 3'),
+    ('Sev 4', 'Sev 4'),
+)
+
--- udt/models.py
+++ udt/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+from udt.choices import REQUEST_TYPE_CHOICES, REQUEST_VIA_CHOICES, GEO_CHOICES, FILTER_BY_TOOL_CHOICES, SEVERITY_CHOICES
 
-# Create your models here.
+
+class Dispatcher(models.Model):
+    request_type = models.CharField(max_length=10, choices=REQUEST_TYPE_CHOICES)
+    requested_via = models.CharField(max_length=20, choices=REQUEST_VIA_CHOICES)
+    geo = models.CharField(max_length=15, choices=GEO_CHOICES)
+    filter_by_tool = models.CharField(max_length=20, choices=FILTER_BY_TOOL_CHOICES)
+    task = models.CharField(max_length=250)
+    assign_to = models.ForeignKey(User, related_name='work_orders')
+    slug = models.SlugField(max_length=50, unique_for_date='task_received')
+    task_received = models.DateTimeField(default=timezone.now)
+    link = models.CharField(max_length=500)
+    severity = models.CharField(max_length=5, choices=SEVERITY_CHOICES)
+    request_contained = models.PositiveIntegerField(default=1)
+    affected_items = models.PositiveIntegerField(default=1)
+    subject = models.CharField(max_length=250)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ('-task_received',)
+
+    def __str__(self):
+        return self.subject
