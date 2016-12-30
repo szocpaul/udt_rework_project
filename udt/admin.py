dd8ebca2b0307e7787f3893f9cbb49a7759415ee
Pal Majoros <pal.majoros@hu.ibm.com>
2016-12-30 12:52:48 +0100

    Customizing the way of admin models

--- udt/admin.py
+++ udt/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Dispatcher
 
-admin.site.register(Dispatcher)
+
+class DispatcherAdmin(admin.ModelAdmin):
+    list_display = (
+        'uid',
+        'filter_by_tool',
+        'assign_to',
+        'task_received',
+        'task',
+        'subject',
+    )
+admin.site.register(Dispatcher, DispatcherAdmin)
